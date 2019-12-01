aabe6a12d49d8c414414dfa5f4b2025b2b0f07cd
Bob <903686208@qq.com>
2019-12-01 14:06:29 +0800

    update

---
+++ src/router/index.js
@@ -0,0 +1,75 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import Home from '@/views/Home.vue'
+import Layout from '@/views/Layout.vue'
+import weather from '@/views/weather.vue'
+import blank from '@/views/blank.vue'
+import Notfound from '@/views/Notfound.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Layout,
+    redirect: "/weather",
+    children: [
+      {
+        path: "/weather",
+        meta: {
+          title: "天气预报"
+        },
+         component: weather
+      },
+    ]
+  },
+  {
+    path: '/blank',
+    name: 'blank',
+    component: Layout,
+    children: [
+      {
+        path: "/blank",
+        meta: {
+          title: "空白页面"
+        },
+         component: blank
+      },
+    ]
+  },
+  
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: "*",
+    meta: {
+      title: "404"
+    },
+    component: Notfound
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+      document.title = to.meta.title;
+  }
+  next();
+
+});
+
+export default router
---
+++ src/views/Layout.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+
+        <SideBar></SideBar>
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <div class="con-wrap" :class="{conCollapse: isCollapse}">
+            <!-- 这是顶部导航 -->
+            <!-- <top-bar></top-bar> -->
+            <!-- 这是组件要插入的地方 -->
+            <router-view class="page-component-wrap animated fadeIn"></router-view>
+        </div>
+    </div>
+</template>
+<script>
+// import TopBar from '@/components/TopBar'
+import SideBar from '@/components/SideBar'
+// import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  name: 'layout',
+  // computed: {
+  //   isCollapse() {
+  //     return this.$store.state.common.isCollapse
+  //   }
+  // },
+  components: {
+    // 'top-bar': TopBar,
+    SideBar
+  }
+}
+</script>
+<style lang="scss" scoped>
+.con-wrap {
+  padding-left: 180px;
+  transition: all 0.3s;
+  h3 {
+    margin-top: 0;
+    color: #324157;
+  }
+  .page-component-wrap {
+    padding: 10px;
+  }
+}
+.con-wrap.conCollapse {
+  padding-left: 64px;
+  transition: all 0.3s;
+}
+</style>
